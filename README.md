f8ad7d8b85b5c1c75879d74c0c5d4884f95057a0
Alessandro Scoccia Pappagallo <alessandro.scoccia.pappagallo@gmail.com>
2016-05-08 11:16:53 +0100

    New version of the script

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ## Description
 
-A simple script to download manga scans from [Otaku Smash](https://www.otakusmash.com/). The script has been written then 7th of February 2016 and the author (me) offers no guarantees that it'll work in the future. Quite the contrary: I can offer almost complete certainty that it won't.
+A simple script to download manga scans from [Otaku Smash](https://www.otakusmash.com/). This version of the script has been written the 08th of May 2016 and the author (me) offers no guarantees that it'll work in the future. Quite the contrary: I can offer almost complete certainty that it won't.
 
 Read the disclaimer (see below) before using the script!
 
@@ -10,14 +10,20 @@ Read the disclaimer (see below) before using the script!
 
 Open the script.py file. There are few parameters to tweak:
 
+	site = 'https://www.otakusmash.com/read-manga/'
     manga = 'Shamo' # Use _ for spaces, e.g. The_Devil_King, case insensitive
     download_from = 34 # Inclusive 
     download_to = 35 # Exclusive
 
-I think it's pretty intuitive, but just in case: *manga* is the name of the manga you want to download (check on [Otaku Smash](https://www.otakusmash.com/) they have the manga you're looking for), *download_from* and *download_to* tell the script which volumes you want to download; in pure Python spirit, *download_from* is inclusive and *download_to* is exclusive.
+I think it's pretty intuitive, but just in case: 
+
+- *site* refers to which site is actually holding the resources as Otaku Smash uses few different domains to store its comics, see for example [mrsmanga](http://www.mrsmanga.com/Cage_of_Eden/001/66/), [otakusmash.com/read-comics/](https://www.otakusmash.com/read-comics/Ultimates/001/) and [otakusmash.com/read-manga/](https://www.otakusmash.com/read-manga/Blade_of_the_Immortal/001/).
+- *manga* is the name of the manga you want to download (check on [Otaku Smash](https://www.otakusmash.com/) they have the manga you're looking for);
+- *download_from* and *download_to* tell the script which volumes you want to download; in pure Python spirit, *download_from* is inclusive and *download_to* is exclusive.
 
 For example to download volumes 3, 4 and 5 of Shamo you'll have to set the parameters to be:
 
+	site = 'https://www.otakusmash.com/read-manga/'
     manga = 'Shamo'
     download_from = 3
     download_to = 6
@@ -25,6 +31,7 @@ For example to download volumes 3, 4 and 5 of Shamo you'll have to set the param
 When you have set up the parameters you are ready to run the script. A new folder with the name of the manga will be created in the same folder where the *script.py* file was placed. Within this folder a new folder for each volume will be created. The structure is:
 
     script.py
+	functions.py
     Shamo
     	Volume 003
     		Page 001
@@ -41,6 +48,10 @@ When you have set up the parameters you are ready to run the script. A new folde
 
 ## Disclaimer
 
-Few considerations: (1) Always check the robots.txt file of a site before scraping content from it, as not everyone is happy to have his or her server hit by multiple consecutive requests; (2) Some mangas uploaded on Otaku Smash may be covered by copyright: please, do make sure what you're doing is legal before doing it. I provide with you the tool, what you do with it it's your sole responsibility. 
+Few considerations: (1) Always check the robots.txt file of a site before scraping content from it, as not everyone is happy to have his or her server hit by multiple consecutive requests; (2) Some mangas uploaded on Otaku Smash may be covered by copyright: please, do make sure what you're doing is legal in your country before doing it. I provide you with the tool, what you do with it it's your sole responsibility. 
 
 And remember, **don't be evil**.
+
+## Acknowledgement
+
+Thanks to @nohance for having spot few bugs!
--- script.py
+++ script.py
@@ -1,72 +1,78 @@
-# Written for Python 3!
+#/usr/bin/env python3
 
+#-----------------------------------
+# Import libraries
+#-----------------------------------
 import urllib.request
 import os
+from bs4 import BeautifulSoup
+from functions import download, pad_zeros
 
-# Parameters
-manga = 'Shamo' # Use _ for spaces, e.g. The_Devil_King, case insensitive
-download_from = 35 # Inclusive
-download_to = 36 # Exclusive
+#-----------------------------------
+# Define parameters
+#-----------------------------------
+site = 'https://www.otakusmash.com/read-manga/'
+# EITHER 'http://www.mrsmanga.com/'
+# OR 'https://www.otakusmash.com/read-comics/'
+# OR 'https://www.otakusmash.com/read-manga/'
+manga = 'Blade_of_the_Immortal' # Use _ for spaces, e.g. The_Devil_King, case insensitive
+download_from = 1 # Inclusive
+download_to = 3 # Exclusive
 # Note: to download the first volume download_from = 1 and download_to = 2
 # Note 2: to download all the volumes just set download_to = 9999
 
-base_path = 'https://www.otakusmash.com/read-manga/mangas/'+manga+'/'
-folder = manga.replace('_',' ').title()
-last_volume = False # When last is reached scrips ends
+#-----------------------------------
+# Let's the download begin!
+#-----------------------------------
+base_path = site+manga+'/'
+folder_name = manga.replace('_',' ').title() 
 
-if not os.path.exists(folder):
-    os.makedirs(folder)
+if not os.path.exists(folder_name):
+    os.makedirs(folder_name) # Create the manga folder
 
-def download(url, path):
-    '''Just a wrapper; download url in path'''
-    urllib.request.urlretrieve(url, path)
-    
-def pad_zeros(string, final_length):
-    '''It adds '0' to string until it reaches final_length length.'''
-    while True:
-        if len(string) == final_length:
-            return string
-        else:
-            string = '0'+string
- 
-for vol in range(download_from, download_to):
+is_last_volume = False # When last is reached scrips ends
+
+for n_volume in range(download_from, download_to):
     
     # Get the volume string
-    str_vol = pad_zeros(str(vol),3)
-    folder_volume = folder+'/Volume '+str_vol
+    str_volume = pad_zeros(str(n_volume),3) # Ex: "001"
+    folder_volume = folder_name+'/Volume '+str_volume # Ex: "Volume 001"
     
     if not os.path.exists(folder_volume):
-        os.makedirs(folder_volume)    
+        os.makedirs(folder_volume) # Create the volume folder
     
-    # Generate the url, part 1
-    url = base_path+str_vol+'/Read-Shamo-Manga-Online-Free-'
+    # Generate the url
+    url = base_path+str_volume
     
-    for page in range(1,999):
+    for page in range(1,9999):
+        # HTML request + souping
+        html = urllib.request.urlopen(url).read() # We get the HTML of the page
+        soup = BeautifulSoup(html, 'html.parser') # We cook it into a soup
+        image_path = soup.find("img", class_="picture")['src'] # Image URL
+        abs_image_path = site+image_path
+        
+        # Naming file
         str_page = pad_zeros(str(page),3)
         filename = folder_volume+'/Page '+str_page+'.jpg'
         
-        try:
-            download(url+str_page+'.jpg',filename)
-            print('Page '+str(page)+' of volume '
-                  +str(vol)+' has been downloaded')
-        except:
-            try: # The last volume's url is different
-                url = base_path+str_vol+'%20-%20THE%20END/Read-Shamo-Manga-Online-Free-'
-                download(url+str_page+'.jpg',filename)
-                print('Page '+str(page)+' of volume '
-                      +str(vol)+' has been downloaded')   
-            except:
-                if page == 1:
-                    # If no page 1 then last volume has been reached
-                    last_volume = True
-                    os.rmdir(folder_volume) # To avoid empty folders
+        # Downloading page
+        download(abs_image_path.replace(" ","%20"),filename)
+        print('Page '+str(page)+' of volume '
+              +str(n_volume)+' has been downloaded')
+        
+        # Search for next page
+        next_url = None
+        tds = soup.find_all(["td"]) # Get all the td
+        for td in tds:
+            list_a = td.find_all("a")
+            for a in list_a:
+                if 'Next Page' in str(a):
+                    next_url = a["href"]
                     break
-                
-                print('Page '+str(page-1)
-                      +' was the last page of volume '+str(vol)+'\n')
-                break
-    
-    if last_volume == True:
-        break
+        
+        if next_url: # There's a next page        
+            url = site+next_url
+        else:
+            break
     
 print('All the volumes requested have been downloaded!')
